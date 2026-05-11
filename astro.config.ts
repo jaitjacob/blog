d8b1a2feccb77eccf9c1944dd4dd34c9b1fed179
Jait Jacob <jai8.jacob@gmail.com>
2026-05-11 19:06:34 +0530

    seo(perf): inline assets (including scripts and images) smaller than 10KB directly into the HTML

--- astro.config.ts
+++ astro.config.ts
@@ -43,6 +43,9 @@ export default defineConfig({
     // This will be fixed in Astro 6 with Vite 7 support
     // See: https://github.com/withastro/astro/issues/14030
     plugins: [tailwindcss()],
+    build: {
+      assetsInlineLimit: 10000,
+    },
     optimizeDeps: {
       exclude: ["@resvg/resvg-js"],
     },
